8783bd8b383fdf75a91ccb636dca42ed10472b05
alkhatuev <74203439+alkhatuev@users.noreply.github.com>
2021-08-03 16:04:04 +0300

    first commit

--- src/pages/Authentication.vue
+++ src/pages/Authentication.vue
@@ -1,20 +1,16 @@
 <template>
     <div class="auth">
-
         <h1>Введите номер телефона</h1>
-       
         <input
         id="authung"
         type="text"
         class='from'
         >
         <button type="button" class="btn btn-outline-light" @click="$router.push({ name: 'films' })">Авторизация</button>
+        <h1 class="card-body">Returned Id: {{articleId}}</h1>
         
     </div>
-
-
 </template>
-
 <style lang="scss" scoped>
     .auth {
         display: flex;
@@ -32,15 +28,27 @@
       margin-bottom: 20px; 
         
     }
-    button {
-    
+    card-body {
+      color: #fff;
     }
     }
     
 </style>
 <script>
-    
-   
+    import axios from "axios";
 
-
+export default {
+  name: "post-request-async-await",
+  data() {
+    return {
+      articleId: null
+    };
+  },
+  async created() {
+    // POST request using axios with async/await
+    const phone = {phone:"89175184542"};
+    const response = await axios.post("http://localhost:3000/phone", phone);
+    this.articleId = response.data.message;
+  }
+};
 </script>
